5fb59527d0c968fa92d0911110d2a530e8dff53d
Bimo Chesta Adabi <BimoChestaAdabi@gmail.com>
2025-03-20 12:47:09 +0700

    Menambahkan ke github dan readme

---
+++ app/src/main/java/com/bimo0064/assesment1_miniproject/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.bimo0064.assesment1_miniproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bimo0064.assesment1_miniproject.ui.theme.Assesment1_miniprojectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Assesment1_miniprojectTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Assesment1_miniprojectTheme {
+        Greeting("Android")
+    }
+}
